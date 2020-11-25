cfb3d416c97a8d8d3c3b31240b0c635d4e635ed1
Luis <rootluis85@gmail.com>
2020-11-24 19:42:45 -0600

    Estructura inicial del proyecto SpringBoot - siest12-rest-api

---
+++ src/main/java/com/edu/mx/rest/est/siest12/Siest12ResApiApplication.java
@@ -0,0 +1,13 @@
+package com.edu.mx.rest.est.siest12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Siest12ResApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Siest12ResApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/mx/rest/est/siest12/Siest12ResApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.mx.rest.est.siest12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Siest12ResApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
